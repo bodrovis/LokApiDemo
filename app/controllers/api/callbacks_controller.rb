7a869e36970d54b46639d754b35dc022ac0739fd
bodrovis <bodrovis@protonmail.com>
2022-08-11 19:02:56 +0300

    finished script

--- app/controllers/api/callbacks_controller.rb
+++ app/controllers/api/callbacks_controller.rb
@@ -3,7 +3,56 @@ module Api
     skip_before_action :verify_authenticity_token, only: [:notify]
 
     def notify
+      if params[:event] == 'project.imported'
+        handle_project_imported
+      elsif params[:event] == 'team.order.completed'
+        handle_order_completed
+      end
+
       head :ok
+    end
+
+    private
+
+    def handle_project_imported
+      project_id = params[:project][:id]
+      key_ids = get_keys_for project_id
+
+      order = api_client.create_order 176692,
+                                      project_id: project_id,
+                                      card_id: 1774,
+                                      briefing: 'MT',
+                                      source_language_iso: params[:language][:iso],
+                                      target_language_isos: %w[fr],
+                                      keys: key_ids,
+                                      provider_slug: 'google',
+                                      translation_tier: 1
+      
+      Order.create order_id: order.order_id, project_id: project_id, status: order.status
+    end
+
+    def handle_order_completed
+      order = Order.find_by order_id: params[:order][:id]
+
+      order.update status: 'completed'
+
+      api_client.download_files order.project_id, format: 'yaml',
+                                original_filenames: true,
+                                filter_langs: %w[fr],
+                                filter_data: %w[translated],
+                                add_newline_eof: true,
+                                triggers: %w[github],
+                                indentation: '2sp',
+                                yaml_include_root: true
+    end
+
+    def get_keys_for(project_id)
+      keys = api_client.keys project_id, limit: 5000,
+                             filter_filenames: "%LANG_ISO%.yml",
+                             filter_translation_lang_ids: 673,
+                             filter_untranslated: 1
+      
+      keys.collection.map(&:key_id)                      
     end
   end
 end
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,8 +1,32 @@
+# Task: whenever a translation file is uploaded on Lokalise,
+# create a new translation order. Then once the order is completed
+# send a PR to a GitHub repo with all the translation data.
+
+# Solution:
+
+# 1. Register webhooks to listen to file
+# uploads and order completion
+
+# 2. When the file is uploaded, automatically
+# create a new translation order with Google MT provider
+
+# 3. When the order is completed, automatically
+# initiate file export and fire GitHub trigger to create a new PR
+
+# 4. Proceed to PRs on GH and merge it! 
+
+# Translation project ID 5403957862e90981d0b0c8.60288957
+# App URL https://lokapidemo.herokuapp.com/
+
 class WebhooksController < ApplicationController
   def new
   end
-  
+
   def create
+    api_client.create_webhook params[:project_id],
+                              url: 'https://lokapidemo.herokuapp.com/api/callbacks/notify',
+                              events: %w[project.imported team.order.completed]
+
     flash[:success] = 'Webhook registered!'
 
     
